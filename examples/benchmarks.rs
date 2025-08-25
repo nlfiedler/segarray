68dfa253e7e01cf564a82d225ad1c775fcda7e63
Nathan Fiedler <nathanfiedler@fastmail.fm>
2025-08-24 20:43:33 -0700

    test: add more benchmarks

--- examples/benchmarks.rs
+++ examples/benchmarks.rs
@@ -2,36 +2,75 @@
 // Copyright (c) 2025 Nathan Fiedler
 //
 use segment_array::SegmentArray;
+use rand::{Rng, SeedableRng, rngs::SmallRng};
 use std::time::Instant;
 
 //
-// This example intends to show that a segment array will grow in less time than
-// a vector, however in practice that may not be the case.
+// This example was intended to show that the segment array will grow in less
+// time than a vector, however in practice that is not the case.
 //
 
-fn create_segarray(size: u64) {
+fn benchmark_segarray(size: usize) {
     let start = Instant::now();
-    let mut coll: SegmentArray<u64> = SegmentArray::new();
+    let mut coll: SegmentArray<usize> = SegmentArray::new();
     for value in 0..size {
         coll.push(value);
     }
     let duration = start.elapsed();
-    println!("segarray: {:?}", duration);
+    println!("segarray create: {:?}", duration);
+
+    // test random access `size` times; use SmallRng to avoid dominating the
+    // running time with random number generation
+    let mut rng = SmallRng::seed_from_u64(0);
+    let start = Instant::now();
+    for _ in 0..size {
+        let index = rng.random_range(0..size);
+        assert_eq!(coll[index], index);
+    }
+    let duration = start.elapsed();
+    println!("segarray random: {:?}", duration);
+
+    // test sequenced access for entire collection
+    let start = Instant::now();
+    for (index, value) in coll.iter().enumerate() {
+        assert_eq!(*value, index);
+    }
+    let duration = start.elapsed();
+    println!("segarray ordered: {:?}", duration);
 }
 
-fn create_vector(size: u64) {
+fn benchmark_vector(size: usize) {
     let start = Instant::now();
-    let mut coll: Vec<u64> = Vec::new();
+    let mut coll: Vec<usize> = Vec::new();
     for value in 0..size {
         coll.push(value);
     }
     let duration = start.elapsed();
-    println!("vector: {:?}", duration);
+    println!("vector create: {:?}", duration);
+
+    // test random access `size` times; use SmallRng to avoid dominating the
+    // running time with random number generation
+    let mut rng = SmallRng::seed_from_u64(0);
+    let start = Instant::now();
+    for _ in 0..size {
+        let index = rng.random_range(0..size);
+        assert_eq!(coll[index], index);
+    }
+    let duration = start.elapsed();
+    println!("vector random: {:?}", duration);
+
+    // test sequenced access for entire collection
+    let start = Instant::now();
+    for (index, value) in coll.iter().enumerate() {
+        assert_eq!(*value, index);
+    }
+    let duration = start.elapsed();
+    println!("vector ordered: {:?}", duration);
 }
 
 fn main() {
     println!("creating SegmentArray...");
-    create_segarray(100_000_000);
+    benchmark_segarray(100_000_000);
     println!("creating Vec...");
-    create_vector(100_000_000);
+    benchmark_vector(100_000_000);
 }
